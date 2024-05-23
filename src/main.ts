d64daedb4b5d6d7d80ee1667f8c686b63f02a1cd
hoonyboom <hyezoprk@kakao.com>
2024-05-23 18:02:43 +0900

    add autoUpdate setting in class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // transformer의 타입 변환, 기본값 할당 허용
+      transformOptions: { enableImplicitConversion: true }, // validator를 기반으로 타입 자동 변환
+    }),
+  );
 
   await app.listen(3002);
 }
