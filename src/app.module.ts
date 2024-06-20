7ed4bf3a4aa454889f7fcfe0b55836a6305e334e
hoonyboom <hyezoprk@kakao.com>
2024-06-21 07:40:25 +0900

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,6 +43,7 @@ import { UsersModule } from './users/users.module';
       useFactory: async (configService: ConfigService) => ({
         store: await redisStore({
           ttl: 60 * 3,
+          //
           socket: {
             host: configService.get<string>(ENV.REDIS_HOST_KEY),
             port: parseInt(configService.get<string>(ENV.REDIS_PORT_KEY)),
