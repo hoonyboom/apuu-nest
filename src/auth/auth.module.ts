b99e1c6108d7802111672c73439ec332500ee834
hoonyboom <hyezoprk@kakao.com>
2024-06-26 18:51:23 +0900

    token interceptor 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { ENV } from 'src/common/const/env.const';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TokenInterceptor } from './interceptor/token.interceptor';
 
 @Module({
   imports: [
@@ -14,6 +16,12 @@ import { AuthService } from './auth.service';
   ],
   exports: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TokenInterceptor,
+    },
+  ],
 })
 export class AuthModule {}
